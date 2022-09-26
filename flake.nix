22ba37cf4cca4c4e6b38ae1135265a8e6f8e93fc
Thomas Wehmöller <connect.mordrag@gmx.de>
2022-09-26 14:24:05 +0200

    ✨ Add rust, python, deno and slides templates

--- flake.nix
+++ flake.nix
@@ -1,33 +1,26 @@
 {
   description = "A collection of flake templates";
 
-  inputs = {
-    rust.url = "github:MordragT/nix-template-rust";
-    python.url = "github:MordragT/nix-template-python";
-    deno.url = "github:MordragT/nix-template-deno";
-    slides.url = "github:MordragT/nix-template-slides";
-  };
-
   outputs = { self, rust, python, deno, slides }: {
 
     templates = {
       rust = {
-        path = "${rust}";
+        path = ./rust;
         description = "Rust development template using fenix";
       };
 
       python = {
-        path = "${python}";
+        path = ./python;
         description = "Python development template using poetry2nix";
       };
 
       deno = {
-        path = "${deno}";
+        path = ./deno;
         description = "Deno runtime development template using deno2nix";
       };
 
       slides = {
-        path = "${slides}";
+        path = ./slides;
         description = "Presentation development template using reveal.js";
       };
 
