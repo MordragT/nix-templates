6c03b0c631b19bade92d7b94d83f91b95621c081
Thomas Wehmöller <connect.mordrag@gmx.de>
2022-10-04 16:29:40 +0200

    ✨ Add kotlin template

---
+++ kotlin/flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "Kotlin development template using gradle";
+
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, ... }:
+
+    utils.lib.eachDefaultSystem (system:
+      let
+        javaVersion = 17;
+
+        overlays = [
+          (self: super: rec {
+            jdk = pkgs."jdk${toString javaVersion}";
+            gradle = super.gradle.override {
+              java = jdk;
+            };
+            kotlin = super.kotlin.override {
+              jre = jdk;
+            };
+          })
+        ];
+
+        pkgs = import nixpkgs { inherit overlays system; };
+      in
+      rec
+      {
+        # # Executed by `nix build`
+        # packages.default = pkgs.stdenv.mkDerivation {
+        #   pname = "template";
+        #   version = "0.1.0";
+        #   src = ./.;
+
+        #   nativeBuildInputs = with pkgs; [
+        #     kotlin
+        #     gradle
+        #   ];
+
+        #   buildPhase = ''
+        #     export GRADLE_USER_HOME=$(mktemp -d)
+        #     export JAVA_HOME=${pkgs.jdk.home}
+
+        #     $src/gradlew build --offline --no-daemon --no-build-cache --no-watch-fs \
+        #       --info --full-stacktrace --warning-mode=all
+        #   '';
+
+        # };
+
+        # # Executed by `nix run`
+        # apps.default = utils.lib.mkApp { drv = packages.default; };
+
+        # Used by `nix develop`
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [ kotlin gradle ];
+
+          shellHook = ''
+            ${pkgs.kotlin}/bin/kotlin -version
+          '';
+
+          JAVA_HOME = pkgs.jdk.home;
+        };
+      });
+}
