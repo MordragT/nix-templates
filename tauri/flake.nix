183662f445171fcfe20770ddc98edb80dfc66a5f
Thomas Wehmöller <connect.mordrag@gmx.de>
2022-10-18 01:43:10 +0200

    🐛 Add rust toolchain components to tauri

--- tauri/flake.nix
+++ tauri/flake.nix
@@ -2,22 +2,33 @@
   description = "Tauri Javascript App";
 
   inputs = {
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils }:
+  outputs = { self, nixpkgs, utils, fenix }:
     utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
-
-        name = "template";
-
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ fenix.overlay ];
+        };
+        toolchain = pkgs.fenix.complete;
         buildInputs = with pkgs; [
           # js
           yarn
 
           # rust
-          cargo
+          (with toolchain; [
+            cargo
+            rustc
+            rust-src
+            clippy
+            rustfmt
+          ])
           pkg-config
           openssl
 
@@ -28,16 +39,10 @@
       in
       rec {
 
-        apps.${name} = utils.lib.mkApp {
-          drv = packages.${name};
-        };
-        apps.default = apps.${name};
-
-        packages.${name} = pkgs.mkYarnPackage {
-          inherit name;
-
-          nativeBuildInputs = buildInputs;
-
+        # Executed by `nix build`
+        packages.default = pkgs.mkYarnPackage rec {
+          inherit buildInputs;
+          name = "template";
           src = ./.;
 
           buildPhase = "yarn build";
@@ -45,13 +50,21 @@
           installPhase = ''
             mkdir -p $out/bin
 
-            cp src-tauri/target/release/tauri-app $out/bin/${name}
+            cp src-tauri/target/release/${name} $out/bin/${name}
           '';
         };
-        packages.default = packages.${name};
 
+        # Executed by `nix run`
+        apps.default = utils.lib.mkApp {
+          drv = packages.default;
+        };
+
+        # Used by `nix develop`
         devShell = pkgs.mkShell {
           inherit buildInputs;
+
+          # Specify the rust-src path (many editors rely on this)
+          RUST_SRC_PATH = "${toolchain.rust-src}/lib/rustlib/src/rust/library";
         };
 
       });
