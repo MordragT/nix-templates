ca76f494d5002881ac73a251015197d6f5678ceb
Thomas Wehmöller <connect.mordrag@gmx.de>
2023-03-26 19:45:42 +0200

    ✨ Add typst template

--- flake.nix
+++ flake.nix
@@ -23,6 +23,45 @@
         '';
       };
 
+      deno = {
+        path = ./deno;
+        description = "Deno runtime development template using deno2nix";
+        welcomeText = ''
+          You have created a Javascript template that will help you manage
+          your Deno project. See the README for instructions on how to
+          use the template.
+        '';
+      };
+
+      kotlin = {
+        path = ./kotlin;
+        description = "Kotlin development template using gradle";
+        welcomeText = ''
+          You have created a Kotlin template that will help you manage
+          your Gradle project. See the README for instructions on how to
+          use the template.
+        '';
+      };
+
+      mdbook = {
+        path = ./mdbook;
+        description = "mdbook template for nix";
+        welcomeText = ''
+          You have created a mdbook template that will help you spin up
+          your book. See the README for further information.
+        '';
+      };
+
+      python = {
+        path = ./python;
+        description = "Python development template using poetry2nix";
+        welcomeText = ''
+          You have created a Python template that will help you manage
+          your Poetry project. See the README for instructions on how to
+          use the template.
+        '';
+      };
+
       rust-stable = {
         path = ./rust-stable;
         description = "Rust development template";
@@ -40,64 +79,6 @@
           You have created a Rust template that will help you manage
           your project. See the README for instructions on how to use
           the template.
-        '';
-      };
-
-      python = {
-        path = ./python;
-        description = "Python development template using poetry2nix";
-        welcomeText = ''
-          You have created a Python template that will help you manage
-          your Poetry project. See the README for instructions on how to
-          use the template.
-        '';
-      };
-
-      kotlin = {
-        path = ./kotlin;
-        description = "Kotlin development template using gradle";
-        welcomeText = ''
-          You have created a Kotlin template that will help you manage
-          your Gradle project. See the README for instructions on how to
-          use the template.
-        '';
-      };
-
-      deno = {
-        path = ./deno;
-        description = "Deno runtime development template using deno2nix";
-        welcomeText = ''
-          You have created a Javascript template that will help you manage
-          your Deno project. See the README for instructions on how to
-          use the template.
-        '';
-      };
-
-      tauri = {
-        path = ./tauri;
-        description = "Tauri development template";
-        welcomeText = ''
-          You have created a Tauri template that will help you manage
-          your Application project. See the README for instructions on
-          how to use the template.
-        '';
-      };
-
-      mdbook = {
-        path = ./mdbook;
-        description = "mdbook template for nix";
-        welcomeText = ''
-          You have created a mdbook template that will help you spin up
-          your book. See the README for further information.
-        '';
-      };
-
-      tex = {
-        path = ./tex;
-        description = "Tex template for nix using tectonic";
-        welcomeText = ''
-          You have created a Tex template that will help you manage your
-          document. See the README for further information.
         '';
       };
 
@@ -131,9 +112,37 @@
         '';
       };
 
+      tauri = {
+        path = ./tauri;
+        description = "Tauri development template";
+        welcomeText = ''
+          You have created a Tauri template that will help you manage
+          your Application project. See the README for instructions on
+          how to use the template.
+        '';
+      };
+
+      tex = {
+        path = ./tex;
+        description = "Tex template for nix using tectonic";
+        welcomeText = ''
+          You have created a Tex template that will help you manage your
+          document. See the README for further information.
+        '';
+      };
+
       trivial = {
         path = ./trivial;
         description = "Simple template using flake-utils";
+      };
+
+      typst = {
+        path = ./typst;
+        description = "Typst template for markup based typewriting";
+        welcomeText = ''
+          You have created a Typst template that will help you manage your
+          document. See the README for further information.
+        '';
       };
     };
   };
---
+++ typst/flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "A typst typewriting flake";
+
+  inputs.utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; }; in rec
+      {
+        devShells.default = pkgs.mkShell rec {
+          buildInputs = with pkgs; [
+            typst
+            just
+          ];
+
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+        };
+      }
+    );
+}
